08fcd2e02e8da24deafd6efd8b72eab1ea7b93f4
rawshad <rawshad@outlook.com>
2022-06-06 12:31:48 +0600

    working on navigation

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,19 +1,4 @@
 
-$(document).ready(function(){
-    $('.js--services-section').waypoint(function(direction){
-        if(direction == 'down') {
-            $('nav').addClass('sticky');
-        } else {
-            $('nav').removeClass('sticky');
-        }
-    }) 
-    var mixer = mixitup('.container');
-});
-
-// var containerEl = document.querySelector('.container');
-
-//             var mixer = mixitup(containerEl);
-
 
 
  
